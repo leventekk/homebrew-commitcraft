a30712b02dbc5d9343c7a34e25747af7f8b5c893
levente kiraly <leventekk@users.noreply.github.com>
2024-04-30 19:48:50 +0200

    feat: first release

---
+++ Formula/commitcraft.rb
@@ -0,0 +1,11 @@
+class CommitCraft < Formula
+  desc "Commit message generator CLI tool written in Rust."
+  homepage "https://github.com/leventekk/commitcraft"
+  url "https://github.com/leventekk/commitcraft/releases/latest/download/commitcraft-aarch64-apple-darwin.tar.gz"
+  sha256 "b60f5085fd77c8dc4fe1a7ecd036e173edf247ce558edbcc1c20e2ffb2bed35e"
+  version "0.9.2"
+
+  def install
+    bin.install "commitcraft"
+  end
+end
---
+++ README.md
@@ -0,0 +1,3 @@
+# homebrew-commitcraft
+
+Homebrew formula for the [commitcraft][https://github.com/leventekk/commitcraft] tool on macOS
