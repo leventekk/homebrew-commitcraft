412e147338190544d9fbac36697654e80f258cb3
levente kiraly <leventekk@users.noreply.github.com>
2024-05-09 23:24:50 +0200

    feat(formula): update Commitcraft to version 0.9.4
    - Update download URL to the latest version
    - Update sha256 checksum for version 0.9.4

--- Formula/commitcraft.rb
+++ Formula/commitcraft.rb
@@ -2,8 +2,8 @@ class Commitcraft < Formula
   desc "Commit message generator CLI tool written in Rust."
   homepage "https://github.com/leventekk/commitcraft"
   url "https://github.com/leventekk/commitcraft/releases/latest/download/commitcraft-aarch64-apple-darwin.tar.gz"
-  sha256 "6c5bcbca68985d6ed1e734ba214dfbf343c3c5df09419460bd27dfffc79b2c90"
-  version "0.9.3"
+  sha256 "df9deeb050d5a3a40eb50d97e82287189c6869dea19cc20232c22ec006170f36"
+  version "0.9.4"
 
   def install
     bin.install "commitcraft"
