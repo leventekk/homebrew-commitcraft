f47ade5bbea925e5f53a8258215e07ab0e7bbbc5
levente kiraly <leventekk@users.noreply.github.com>
2024-05-16 13:31:29 +0200

    feat(formula): update Commitcraft to version 0.9.5

--- Formula/commitcraft.rb
+++ Formula/commitcraft.rb
@@ -2,8 +2,8 @@ class Commitcraft < Formula
   desc "Commit message generator CLI tool written in Rust."
   homepage "https://github.com/leventekk/commitcraft"
   url "https://github.com/leventekk/commitcraft/releases/latest/download/commitcraft-aarch64-apple-darwin.tar.gz"
-  sha256 "df9deeb050d5a3a40eb50d97e82287189c6869dea19cc20232c22ec006170f36"
-  version "0.9.4"
+  sha256 "4fd5f406e0ebb98a63a822e7caf892b4bdc45cdcde8e708c03c3f591b8b28db5"
+  version "0.9.5"
 
   def install
     bin.install "commitcraft"
